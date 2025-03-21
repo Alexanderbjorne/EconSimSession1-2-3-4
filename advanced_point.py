d8b2236f59da6723e4c175f0601be76ff2206d6e
alexanderbjorne <abjrne.ieu2023@student.ie.edu>
2025-03-21 11:03:49 +0100

    Added point, Color point, and advanced point.

---
+++ advanced_point.py
@@ -0,0 +1,7 @@
+from color_point import ColorPoint
+
+class AdvancedPoint(ColorPoint): #This means that advanced point inherit everything from Colorpoint
+    pass
+
+p = AdvancedPoint(1, 2, "red")
+

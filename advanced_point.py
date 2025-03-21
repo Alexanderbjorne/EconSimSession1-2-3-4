74986bf3c6db589f8e4ec0a2b85317639e0bad50
alexanderbjorne <abjrne.ieu2023@student.ie.edu>
2025-03-21 11:50:51 +0100

    Added @classmethod @property and @color.setter, Break.

--- advanced_point.py
+++ advanced_point.py
@@ -1,7 +1,61 @@
 from color_point import ColorPoint
 
 class AdvancedPoint(ColorPoint): #This means that advanced point inherit everything from Colorpoint
-    pass
+    COLORS = ["red", "green", "blue", "black", "white"]
+    def __init__(self, x, y, color):
+      if not isinstance(x,(int, float)):
+          raise TypeError("x must be a number")
+      if not isinstance(y,(int, float)):
+        raise TypeError("y must be a number")
+      if not color in self.COLORS:
+          raise ValueError(f"color must be one of: {self.COLORS}")
+      # super().__init__(x, y, color) #Call the init method of the parent
+      self._x = x
+      self._y = y
+      self._color = color
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, new_color):
+        if new_color not in AdvancedPoint.COLORS:
+            raise ValueError(f"color must be one of: {self.COLORS}")
+        self._color = new_color
+
+    @classmethod
+    def add_color(cls, new_color):
+        cls.COLORS.append(new_color)
 
 p = AdvancedPoint(1, 2, "red")
+print(p)
+print(p.distance_orig())
+
+# p = AdvancedPoint("jon", "jeb", "blue")
+# print(p)
+# print(p.distance_orig())
+
+AdvancedPoint.add_color("amber")
+p2 = AdvancedPoint(9, 3, "amber")
+# print(p2)
+# print(p2.distance_orig())
+# print(p2.color)
+# p2.color = "Bob"
+# print(p2.color)
+
+p2.color = "blue"
+
+print(p2)
+# print(p2.x)
+# print(p2.y)
+# print(p2.color)
 
--- color_point.py
+++ color_point.py
@@ -13,16 +13,16 @@ class ColorPoint(Point):
     def __str__(self):
         return f"<{self.x},{self.y}>({self.color})"
 
-color_points = []
-colors = ["red", "blue", "green", "yellow", "black", "white", "purple"]
-for _ in range(5):
-    p = ColorPoint(
-        random.randint(-100, 100),
-        random.randint(-100, 100),
-        random.choice(colors))
-    color_points.append(p)
-print("random color points:")
-print(color_points)
-color_points.sort()
-print("color points in order:")
-print(color_points)
+# color_points = []
+# colors = ["red", "blue", "green", "yellow", "black", "white", "purple"]
+# for _ in range(5):
+#     p = ColorPoint(
+#         random.randint(-100, 100),
+#         random.randint(-100, 100),
+#         random.choice(colors))
+#     color_points.append(p)
+# print("random color points:")
+# print(color_points)
+# color_points.sort()
+# print("color points in order:")
+# print(color_points)
